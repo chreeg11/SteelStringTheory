9ec86aac835530c718c9a82c7ea07b724aef447f
Chris Graig <cgraig@gmail.com>
2026-02-28 10:14:17 -0800

    Sart adding chords

---
+++ MusicTheory/Models/Chord.cs
@@ -0,0 +1,35 @@
+namespace MusicTheory.Models
+{
+    public enum ChordType
+    {
+        Major,
+        Minor,
+        Dim,
+        Aug,
+        Maj7,
+        Min7,
+        Dom7,
+        Dim7,
+    }
+
+    public static class ChordPatterns
+    {
+        private static readonly Dictionary<ChordType, int[]> Patterns = new()
+        {
+            // Semitone offsets from root (NOT step-by-step)
+            { ChordType.Major, [4, 7] },
+            { ChordType.Minor, [3, 7] },
+            { ChordType.Aug, [4, 8] },
+            { ChordType.Dim, [3, 6] },
+            { ChordType.Maj7, [4, 7, 11] },
+            { ChordType.Min7, [3, 7, 10] },
+            { ChordType.Dom7, [4, 7, 10] },
+            { ChordType.Dim7, [3, 6, 9] },
+        };
+
+        public static int[] GetPattern(ChordType chordType) => Patterns[chordType];
+
+        public static bool TryGetPattern(ChordType chordType, out int[] pattern)
+            => Patterns.TryGetValue(chordType, out pattern!);
+    }
+}
