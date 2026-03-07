c38331a05a3695965fc18813909fbf01edaa2a3a
Chris Graig <cgraig@gmail.com>
2026-03-07 11:18:17 -0800

    Fix braces

--- PedalSteel/Models/Copedent.cs
+++ PedalSteel/Models/Copedent.cs
@@ -14,10 +14,16 @@ namespace PedalSteel.Models
             var validNumbers = new HashSet<uint>(strings.Select(s => s.Number));
 
             foreach (var (pedal, effects) in pedalEffects)
+            {
                 foreach (var effect in effects)
+                {
                     if (!validNumbers.Contains(effect.StringNumber))
+                    {
                         throw new ArgumentException(
                             $"Pedal '{pedal.Name}' references string {effect.StringNumber}, which does not exist in this copedent.");
+                    }
+                }
+            }
 
             Name = name;
             Strings = strings;
